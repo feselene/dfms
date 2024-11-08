bde676b1b21cb65522684fceb9a9466de0f49744
feselene <vaporflyb@gmail.com>
2024-11-08 14:44:03 -0500

    change postgres login creds

--- backend/settings.py
+++ backend/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'new_database_name',    # Replace with your database name
-        'USER': 'postchess',      # Replace with your PostgreSQL username
+        'USER': 'postgres',      # Replace with your PostgreSQL username
         'PASSWORD': 'lichess',  # Replace with your PostgreSQL password
         'HOST': 'localhost',             # Or the IP address of your PostgreSQL server
         'PORT': '5432',                  # Default PostgreSQL port, change if needed
